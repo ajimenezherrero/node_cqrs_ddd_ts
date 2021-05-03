3208ad80f123fb2299a47cf72ce2b0f9d24ee9bf
alejandro-jimenez <alejandro.jimenez@xing.com>
2021-05-03 09:26:16 +0200

    Add logger

---
+++ src/shared/infrastructure/logger/WinstonLogger.ts
@@ -0,0 +1,28 @@
+import winston from 'winston';
+
+
+class WinstonLogger {
+  instance: any;
+  loggerLevel: string = 'debug';
+  
+  constructor(loggerLevel: string) {
+    this.loggerLevel = loggerLevel;
+  }
+
+  get logger() {
+    if (!this.instance) {
+      this.instance = winston.createLogger({
+        level: this.loggerLevel,
+        transports: [
+          new winston.transports.Console({
+            format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+          }),
+        ],
+      });
+    }
+
+    return this.instance;
+  }
+}
+
+export default WinstonLogger;
